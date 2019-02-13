bde5770b26f244a20372bf30b57c3315dfdc05b1
ps1337 <pschmied@mailbox.org>
2019-02-13 13:49:42 +0100

    update helpers

---
+++ helpers/upload0r_line_by_line.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Upload a file in small chunks using echo commands
+# Usage: python3 upload0r.py [file]
+# author: pschmied
+
+import sys
+import random
+import string
+import base64
+import re
+
+CHUNKSIZE = 40
+
+filename = sys.argv[1]
+
+# random tag
+rnd = ''.join(
+    random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+
+with open(filename, "rb") as f:
+    encoded = base64.b64encode(f.read()).decode()
+
+
+chunks = re.findall('.' * CHUNKSIZE, encoded)
+
+for chunk in chunks:
+    print("echo -ne '%s' >> /tmp/.yolofile_%s && \\" % (chunk, rnd))
+
+print("cat /tmp/.yolofile_%s | base64 -d > /tmp/yolofile_%s && \\" % (rnd, rnd))
+print("rm /tmp/.yolofile_%s" % (rnd))
--- helpers/upload0r_whole_file.py
+++ helpers/upload0r_whole_file.py
@@ -16,12 +16,6 @@ filename = sys.argv[1]
 rnd = ''.join(
     random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
 
-with open(filename) as f:
-    lines = f.readlines()
-
-for line in lines[:-1]:
-    encoded = base64.b64encode(line.encode()).decode()
-    print("echo '%s' | base64 -d >> /tmp/yolofile_%s && \\" % (encoded, rnd))
-
-print("echo '%s' | base64 -d >> /tmp/yolofile_%s" %
-      (base64.b64encode(lines[-1].encode()).decode(), rnd))
+with open(filename, "rb") as f:
+    encoded = base64.b64encode(f.read()).decode()
+print("echo '%s' | base64 -d >> /tmp/yolofile_%s" % (encoded, rnd))
